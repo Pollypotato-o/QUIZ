e40e69f4b598dfeb57f11bda464b29413d110f1e
Pollypotato-o <polina_daughter_of_dmitry@mail.ru>
2023-03-23 15:57:48 +0300

    registration v1

--- frontend/src/features/auth/types/Action.ts
+++ frontend/src/features/auth/types/Action.ts
@@ -1,4 +1,8 @@
+import { Message, User } from './types';
+
 type Action =
-{}
+   { type: 'AUTHORIZATION'; payload:User | Message }
+ | { type: 'REGISTRATION'; payload:User | Message }
+ | { type:'VERIFICATION'; payload:User | Message };
 
 export default Action;
